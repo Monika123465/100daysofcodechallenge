dfb1f3b3a1dd1e51f603c911e604d3c32ebae061
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-06-12 10:50:39 +0530

    Time: 159 ms (91.96%), Space: 50.5 MB (89.88%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,28 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    // let xn=x.toString()
-    // let bag=""
-    // for(let i=xn.length;i>0;i--){
-    //     bag+=xn[i]
-    // }
-    // if(bag==xn){
-    //     return true
-    // }else{
-    //     return false
-    // }
-    
-    const isnegative=x<0 ?true:false
-    if(isnegative){
-        return false
+     let xn=x.toString()
+    let bag=""
+    for(let i=xn.length - 1;i>=0;i--){
+    bag+=xn[i]
+    }
+    if(bag==xn){
+    return true
+    }else{
+    return false
     }
     
-    const temp=x
-    let reversed=0
-    while(x>0){
-        reversed=(reversed*10)+(x%10)
-        x=parseInt(x/10)
-    }
-    return reversed==temp
+   
     
 };

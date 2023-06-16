d7e3be37b399d68c0af8f9e0bd9756c050107cd4
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-06-16 11:42:55 +0530

    Time: 134 ms (26.42%), Space: 42.5 MB (57.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,14 +5,14 @@
  */
 var twoSum = function(nums, target) {
           
-         let bag=[] 
-         for(let i=0;i<nums.length;i++){
-             for(let j=i+1;j<nums.length;j++){
-                 if(nums[i]+nums[j]===target){
-                     bag=[i,j]
-                 }
-             }
-         }
-         return bag
+         let bag=[]
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                bag=[i,j]
+            }
+}
+}
+    return bag
     
 };

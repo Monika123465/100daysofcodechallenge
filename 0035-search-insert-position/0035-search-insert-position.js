36e905e5900a2be3e028da65c3332245119d4eec
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-06-19 13:11:17 +0530

    Time: 64 ms (26.82%), Space: 42.1 MB (65.81%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -9,16 +9,14 @@ let start=0
 let end=nums.length-1
 let mid=0
 while(start<=end){
-    mid=Math.floor((start+end) /2)
+   mid=Math.floor((start+end)/2)
     if(nums[mid]==target) return mid
-    if(nums[mid]<target) 
+    if(nums[mid]<target)
         start=mid+1
-    else 
-        end=mid-1
+    else end=mid-1
 }
-if(nums[mid]>target) return mid
-      return mid+1
-    
+  if(target<nums[mid])   return mid
+    return mid+1
 }
 
 

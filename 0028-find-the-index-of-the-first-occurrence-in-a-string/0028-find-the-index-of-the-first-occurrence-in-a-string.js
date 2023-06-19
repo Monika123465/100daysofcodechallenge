1129a6e666ab5908c5776b29d7c96fb956756c0a
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-06-19 12:46:07 +0530

    Time: 54 ms (80.69%), Space: 41 MB (99.37%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,14 +4,15 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-   if(!needle.length) return 0
-   for(let i=0;i<haystack.length;i++){
-         let k=i ,j=0
-         while(haystack[k]==needle[j] && j<needle.length){
-             k++,j++
-         }
-       if(j==needle.length)return i
-
+ 
+if(needle.length==0) return 0
+    for(let i=0;i<haystack.length;i++){
+        let k=i
+        let j=0
+        while(haystack[k]==needle[j]&& j<needle.length){
+            k++,j++
+        }
+        if(j==needle.length) return i
     }
     return -1
 };

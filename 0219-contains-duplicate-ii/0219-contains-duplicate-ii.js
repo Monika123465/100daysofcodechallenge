23cde428c65b69806ce9dd912c382d1520ba9e40
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-06-21 10:54:39 +0530

    Time: 9330 ms (5.16%), Space: 50.1 MB (97.07%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+   for(let i=0;i<nums.length;i++) {
+       for(let j=0;j<i;j++){
+           if(nums[i]==nums[j]&& Math.abs(i-j)<=k){
+               return true
+           }
+       }
+   }
+    return false
+};

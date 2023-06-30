3fdaee8dd6b460b0de52d1486fc75f98a5565304
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-06-30 10:21:24 +0530

    Time: 1398 ms (5.04%), Space: 179.1 MB (14.28%) - LeetHub

--- 0204-count-primes/0204-count-primes.js
+++ 0204-count-primes/0204-count-primes.js
@@ -3,28 +3,27 @@
  * @return {number}
  */
 var countPrimes = function(n) {
-   
-      if( n <= 2 ) {
-        return 0;
+ 
+    if(n<=2){
+        return 0
+    }
+    let primes=[]
+    let i=0
+    let j=0
+    let count=0
+    
+    for(i=2;i<n;i++){
+        primes[i]=true
     }
 
-    let primes = [];
-    let i, j, count = 0;
-
-    for( i = 2; i < n; i++ ){
-        primes[i] = true;
+    for(i=2;i<=Math.sqrt(n);i++){
+        if(primes){
+            for(j=i+i;j<n;j+=i)
+                primes[j]=false
+}
     }
-
-    for( i = 2; i <= Math.sqrt(n); i++ ){
-        if( primes[i] ){
-            for( j = i + i; j < n; j += i )
-                primes[j] = false;
-        }
-    }
-
-    for( i = 2; i < n; i++ )
-        if( primes[i] )
-            count++;
-
-    return count;
+    for(i=2;i<n;i++)
+        if(primes[i])
+            count++
+    return count
 };

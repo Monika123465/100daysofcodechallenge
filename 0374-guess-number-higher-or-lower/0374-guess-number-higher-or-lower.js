cab9312d290e1ef67517602f327c65be39f19d4f
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-07-06 09:48:58 +0530

    Time: 51 ms (85.99%), Space: 41.6 MB (60.71%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -18,9 +18,14 @@ var guessNumber = function(n) {
     while(left < right) {
         const mid = left + Math.floor((right-left)/2)
         const current = guess(mid)
-        if(current === 0) return mid
-        if(current === -1) right = mid
-        else left = mid+1
+        if(current === 0)
+            return mid
+        if(current === -1) {
+             right = mid
+            } else{
+                
+                left = mid+1
+            } 
     }
     return left
 };

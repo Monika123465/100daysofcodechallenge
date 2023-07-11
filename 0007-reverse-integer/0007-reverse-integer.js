7c97141cbee2ed6ee30cbd73a287636f89c16ae0
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-07-11 09:47:56 +0530

    Time: 59 ms (95.45%), Space: 43.9 MB (55.74%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -8,10 +8,10 @@ var reverse = function(x) {
     let max= Math.pow(2, 31) - 1
     while (x !== 0) {
         let last = x % 10;     
-        x  = parseInt(x / 10);  
-        result = (result * 10) + last;
+        x  = parseInt(x / 10)
+        result = (result * 10) + last
         if (result < min || result > max) return 0;
     }
-    return result;
+    return result
     
 };

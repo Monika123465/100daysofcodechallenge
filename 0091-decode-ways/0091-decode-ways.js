8c0f9ad6485f1fdaadbbdc2270b36fd98b9009a8
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-07-14 10:36:33 +0530

    Time: 64 ms (62.89%), Space: 41.7 MB (95.47%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    
+    let  dp = Array(s.length+1).fill(0)
+      if(s[0]==='0') return 0
+    
+    dp[0] = 1
+    dp[1] = 1
+
+    for(let i = 2; i <= s.length; i++) {
+        
+        let oneDigit = +s.slice(i-1, i)
+        let twoDigits = +s.slice(i-2, i)
+        
+        if(oneDigit > 0) dp[i] = dp[i-1]
+        if(twoDigits >= 10 && twoDigits <= 26) dp[i] += dp[i-2]
+    }    
+    return dp[s.length]
+    
+};

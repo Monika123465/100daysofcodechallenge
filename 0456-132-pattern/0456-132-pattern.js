d695c4d4395dd72084a410f3dcf2bf974a122b43
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-07-23 16:02:38 +0530

    Time: 62 ms (100.00%), Space: 56.2 MB (13.51%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function(nums) {
+        if (nums.length < 3) return false
+  
+  let num = -Infinity
+  let stack = []
+  
+  
+  for (let i =nums.length - 1; i > -1; i--) {
+    if (nums[i] < num) return true
+    else {
+      while (stack.length && stack[stack.length - 1] < nums[i]) {
+        num = stack.pop()
+      }
+    }
+    stack.push(nums[i])
+  }
+  
+  return false
+
+};

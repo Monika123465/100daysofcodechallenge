5775f263c67c9c880f26889356d7d396147a3624
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-07-24 21:09:08 +0530

    Time: 108 ms (51.89%), Space: 51.8 MB (65.09%) - LeetHub

--- 0089-gray-code/0089-gray-code.js
+++ 0089-gray-code/0089-gray-code.js
@@ -12,7 +12,7 @@ let code = function (n, res, num) {
   if (num >= n) return
   let len = res.length
   for (let i = len - 1; i >= 0; i--) {
-    res.push(res[i] + len);
+    res.push(res[i] + len)
   }
   code(n, res, num + 1)
 };

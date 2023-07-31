a56ad154888662daa6f0551037d2f018b67bfe78
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-07-31 20:47:38 +0530

    Time: 55 ms (92.26%), Space: 42.1 MB (90.34%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+   
+     if (!nums.length) return 0
+  let localMax = 0
+  let localMin = 0
+  let lastMax = nums[0]
+  let lastMin = nums[0]
+  let max = nums[0]
+  for (var i = 1; i < nums.length; i++) {
+    localMax = Math.max(lastMax * nums[i], lastMin * nums[i], nums[i])
+    localMin = Math.min(lastMax * nums[i], lastMin * nums[i], nums[i])
+    max = Math.max(max, localMax);
+    lastMax = localMax;
+    lastMin = localMin;
+  }
+  return max;
+};
